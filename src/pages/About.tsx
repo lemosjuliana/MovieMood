535159079657ab311163e8ccf90a55747c95c7f4
Juliana Lemos <lemos.julianajesus@gmail.com>
2025-10-07 14:16:14 -0500

    fixing some inconsistences+about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import aboutImg from "../assets/about.jpg";
+import "../components/Layout/AboutPage.css";
 
 const About: React.FC = () => {
   return (
     <div>
       <main style={{ padding: "40px", fontFamily: "'Open Sans', sans-serif", color: "white" }}>
-        <h1>Welcome to About!</h1>
-        <p>This is your About page placeholder.</p>
+        <h1>Welcome to Movie Mood!</h1>
+        <p>Have you ever planned a movie night with family or friends, only to spend ages trying to find a movie worth watching?</p>
+        <p>The reality is that factors like our mood, the people we're watching with, and the time available to watch can make choosing a movie frustrating and time-consuming. Movie Mood is an app that makes it easier to select content worth watching based on your mood, your time availability, and more.</p>
+        <p>With Movie Mood, you can find a movie that matches your mood. Anywhere. Anytime.</p>
+  <img className="about-img" src={aboutImg} alt="About Image" />
       </main>
     </div>
   );
